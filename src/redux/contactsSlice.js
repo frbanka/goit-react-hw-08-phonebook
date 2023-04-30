a124b6e980289e5de84bf4e0dd265098f6238c27
Jacek Wójciak <fr.banka@gmail.com>
2023-04-30 15:03:55 +0200

    up final

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,5 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit';
-const STORAGE_KEY = 'myContacts';
 
 const firstState = { items: [] };
 const contactsSlice = createSlice({
@@ -8,18 +7,12 @@ const contactsSlice = createSlice({
   reducers: {
     createContact(state, action) {
       state.items.push(action.payload);
-      setLocalStorage(state);
     },
     removeContact(state, action) {
       state.items = state.items.filter(item => item.id !== action.payload);
-      setLocalStorage(state);
     },
   },
 });
 
 export const { createContact, removeContact } = contactsSlice.actions;
 export const contactsReducer = contactsSlice.reducer;
-
-export const setLocalStorage = state => {
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(state));
-};
